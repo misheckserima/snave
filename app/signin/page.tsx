dcad90986e3dffb8653c2d9f54b8b108d57a0af0
ahsurserima <stoneyboyserima@gmail.com>
2025-07-25 00:54:40 +0200

    last

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -195,7 +195,7 @@ export default function SignIn() {
 
           <div className="mt-6 text-center">
             <p className="text-gray-600">
-              Don\'t have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link href="/" className="text-blue-600 hover:text-blue-700 font-medium">
                 Register here
               </Link>
