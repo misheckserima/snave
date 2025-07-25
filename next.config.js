84738b0cbc0877c08e2dae6ed92f36af77c85f12
ahsurserima <stoneyboyserima@gmail.com>
2025-07-25 03:14:08 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   basePath: '/app',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://lingualearner-api.onrender.com/api/:path*'
+      }
+    ]
+  }
 }
 
 /** @type {import('next').NextConfig} */
